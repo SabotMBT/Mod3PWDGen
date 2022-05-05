d9b2f91ada63d25aa8b0407e1f320f0144f25cb0
SabotMBT <104104069+SabotMBT@users.noreply.github.com>
2022-05-05 16:07:39 -0600

    Ver 1.0 (Functionality Achieved!)

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Password Generator Starter Code
+
 # Mod3PWDGen
+
+In order to protect sensitive data, this application allows the user to randomly generate a strong password that meets certain criteria in order to provide enhanced security.
+
+To start, click the "Generate" button. Follow the prompts to specify the password criteria. Once finished, the generated password will be displayed on screen.
+
+Ver 1.0: Functionality achieved.
+
+- Defined 3 character lists of increasing complexity
+- Created prompt to allow user to choose which character list they want to use
+- Created prompt to allow user to choose how long they want their password to be
+- Used iteration and a random number generator to select random characters equal to the chosen password length
+- Linked newly generated password variable to on-screen text box
+
+Ver 0.1: Initial upload
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,43 @@
 // Assignment code here
 
+function generatePassword() {
+  var pwdChar = prompt("Enter A to only use upper and lower case letters. '\n' Enter 1 to include numbers. '\n' Enter ! to include special characters.");
+  console.log(pwdChar);
+  var charList = "abcdefghijklmnopqrstuvwxyxABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  if (pwdChar === "1") {
+    var charList = "abcdefghijklmnopqrstuvwxyxABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  }
+
+  else if (pwdChar === "!") {
+    var charList = "abcdefghijklmnopqrstuvwxyxABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+  }
+  else {
+    var charList = "abcdefghijklmnopqrstuvwxyxABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  var pwdLength = 8;
+  var password = "";
+  var lengthSelect = prompt("Please select a number between 8 and 128.")
+  console.log(lengthSelect)
+
+  if (lengthSelect >= 8 && lengthSelect <= 129) {
+    for (var i = 0; i < lengthSelect; i++) {
+      var randomNumber = Math.floor(Math.random() * charList.length);
+      password += charList.substring(randomNumber, randomNumber + 1);
+    }
+  }
+  else {
+    for (var i = 0; i < pwdLength; i++) {
+      var randomNumber = Math.floor(Math.random() * charList.length);
+      password += charList.substring(randomNumber, randomNumber + 1);
+    }
+  }
+  console.log(pwdLength);
+  console.log(password);
+  window.password = password
+}
+console.log(window.password)
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -9,7 +46,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = window.password;
 
 }
 
